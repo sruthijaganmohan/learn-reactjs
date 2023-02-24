d07c50583220ee9098393519a4e7270463eb713c
sruthijaganmohan <sruthi12jagan@gmail.com>
2023-02-24 20:11:16 +0530

    movie displayer using react

---
+++ src/App.js
@@ -0,0 +1,71 @@
+import React, { useLayoutEffect } from "react";
+import { useEffect, useState } from "react";
+import './App.css';
+import SearchIcon from './search.svg'
+import MovieCard from "./MovieCard";
+
+const API_URL = "http://www.omdbapi.com?apikey=134c4e2a"
+
+const movie1 = {
+    "Title": "Spiderman",
+    "Year": "2010",
+    "imdbID": "tt1785572",
+    "Type": "movie",
+    "Poster": "N/A"
+}
+
+const App = () => {
+    const [movies, setMovies] = useState([]);
+    const [searchTerm, setSearchTerm] = useState('');
+
+    const searchMovies = async (title) => {
+        const response = await fetch(`${API_URL}&s=${title}`)
+        const data = await response.json();
+
+        setMovies(data.Search);
+    }
+
+    useEffect(() => {
+        searchMovies('Spiderman');
+    }, []);
+
+    return (
+        <div className="app">
+            <h1>Moviesss</h1>
+
+            <div className="search">
+                <input 
+                    placeholder="Search for movies" 
+                    value={searchTerm}
+                    onChange={(e) => setSearchTerm(e.target.value)}>
+                </input>
+                    
+                <img 
+                    src={SearchIcon} 
+                    alt="search"
+                    onClick={() => searchMovies(searchTerm)}>
+                </img>
+            </div>
+
+            {
+                movies?.length > 0
+                ? (
+                    <div className="container">
+                        {movies.map((movie) => (
+                            <MovieCard movie={movie} />
+                        ))}
+                    </div>
+                )
+                : (
+                    <div className="empty">
+                        <h2>No movies found</h2>
+                    </div>
+                )
+            }
+
+            
+        </div>
+    );
+}
+
+export default App;
